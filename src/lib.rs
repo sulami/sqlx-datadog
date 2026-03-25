3f683bffd25ebce8b24ba7e79cc740eabfe8784b
Robin Schroer <git@sulami.xyz>
2026-03-25 09:33:34 +0900

    feat: Set query span tags automatically
    This works as long as there is a literal string binding called `query`
    in the function.

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,29 @@ use syn::{Meta, parse_macro_input, punctuated::Punctuated};
 ///
 /// Accepts all arguments `tracing::instrument` accepts, but patches in extra fields.
 ///
-/// By default, expects a function argument called `db` that has a reference to the connection, but
-/// accepts a `db` parameter with an alternative identifier.
+/// By default, expects a function argument called `db` that has a reference to the database
+/// connection.
 ///
-/// For optimal results, the `db.statement` span tag should be set to the text of the SQL query
-/// executed.
+/// If there is a literal string binding called `query` present, its value will be used to set the
+/// relevant span tags.
+///
+/// The names of the connection and query binding can be changed using macro parameters, e.g.:
+///
+/// ```
+/// # #[macro_use] extern crate sqlx_datadog;
+/// # use sqlx::Execute;
+/// #
+/// # #[derive(Debug, sqlx::FromRow)]
+/// # struct User { name: String, email: String }
+/// #
+/// #[instrument_query(skip(conn), db = conn, query = my_query)]
+/// async fn fetch_user(conn: &sqlx::MySqlPool, user_id: i64) -> Result<User, sqlx::Error> {
+///     let my_query = "SELECT name, email FROM users WHERE id = ? LIMIT 1";
+///     sqlx::query_as(my_query).bind(user_id).fetch_one(conn).await
+/// }
+/// ```
+///
+/// # Example
 ///
 /// ```
 /// # #[macro_use] extern crate sqlx_datadog;
@@ -23,9 +41,8 @@ use syn::{Meta, parse_macro_input, punctuated::Punctuated};
 /// #
 /// #[instrument_query(skip(db))]
 /// async fn fetch_user(db: &sqlx::MySqlPool, user_id: i64) -> Result<User, sqlx::Error> {
-///     let query = sqlx::query_as("SELECT name, email FROM users WHERE id = ? LIMIT 1");
-///     tracing::Span::current().record("db.statement", query.sql().trim());
-///     query.bind(user_id).fetch_one(db).await
+///     let query = "SELECT name, email FROM users WHERE id = ? LIMIT 1";
+///     sqlx::query_as(query).bind(user_id).fetch_one(db).await
 /// }
 /// ```
 #[proc_macro_attribute]
@@ -36,11 +53,14 @@ pub fn instrument_query(args: TokenStream, item: TokenStream) -> TokenStream {
     let mut instrument_args: Vec<Meta> = vec![];
     let mut fields = vec![];
     let mut db_ident = quote! { db };
+    let mut query_ident = "query".to_string();
 
     for arg in args {
         if let Meta::NameValue(name_value) = arg.clone() {
             if name_value.path.get_ident().unwrap() == "db" {
                 db_ident = name_value.value.into_token_stream();
+            } else if name_value.path.get_ident().unwrap() == "query" {
+                query_ident = name_value.value.into_token_stream().to_string();
             } else {
                 instrument_args.push(arg);
             }
@@ -55,8 +75,22 @@ pub fn instrument_query(args: TokenStream, item: TokenStream) -> TokenStream {
         }
     }
 
+    // Check if there's a `let query = "..."` binding
+    let mut query_literal = None;
+    for stmt in &input_fn.block.stmts {
+        if let syn::Stmt::Local(local) = stmt &&
+            let syn::Pat::Ident(pat_ident) = &local.pat &&
+            pat_ident.ident == query_ident &&
+            let Some(init) = &local.init &&
+            let syn::Expr::Lit(expr_lit) = &*init.expr &&
+            let syn::Lit::Str(lit_str) = &expr_lit.lit {
+                query_literal = Some(lit_str.clone());
+                break;
+        }
+    }
+
     // These are in reverse.
-    let injected_tags = vec![
+    let mut injected_tags = vec![
         quote! { ::tracing::Span::current().record("peer.hostname", #db_ident.connect_options().get_host()); },
         quote! { ::tracing::Span::current().record("out.host", #db_ident.connect_options().get_host()); },
         quote! { ::tracing::Span::current().record("out.port", #db_ident.connect_options().get_port()); },
@@ -65,11 +99,14 @@ pub fn instrument_query(args: TokenStream, item: TokenStream) -> TokenStream {
         quote! { ::tracing::Span::current().record("db.system", #db_ident.connect_options().to_url_lossy().scheme().replace("postgres", "postgresql")); },
         quote! { use ::sqlx::ConnectOptions; },
     ];
+
+    if let Some(query_lit) = query_literal {
+        injected_tags.insert(0, quote! { ::tracing::Span::current().record("db.statement", #query_lit.trim()); });
+        injected_tags.insert(0, quote! { ::tracing::Span::current().record("resource", #query_lit.trim()); });
+    }
+
     for tag in injected_tags {
-        input_fn
-            .block
-            .stmts
-            .insert(0, syn::parse(tag.into()).unwrap());
+        input_fn.block.stmts.insert(0, syn::parse(tag.into()).unwrap());
     }
 
     let instrument_attr = quote! {
